039aa88d0a752b7ba3682c2dd7e71626da97063a
David Flindall <david@backtofront.london>
2017-08-02 16:26:01 +0100

    Added the main holding page view

---
+++ views/holding.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Maintenance mode template that's shown to logged out users.
+ *
+ * @package   wordpress-holding-page
+ * @copyright Copyright (c) 2015, Ashley Evans
+ * @license   GPL2+
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<link rel="stylesheet" href="<?php echo plugins_url( 'assets/css/maintenance.css', dirname( __FILE__ ) ); ?>">
+
+	<title>Site Coming Soon</title>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
